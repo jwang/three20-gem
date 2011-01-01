078080243bde174859881b9e1eb939867e607205
John Wang <john@johntwang.com>
2011-01-01 00:21:31 -0500

    guid

--- lib/three20/pbxproj.rb
+++ lib/three20/pbxproj.rb
@@ -27,6 +27,9 @@ module Three20
         Raise XCodeError
       else
         puts "project: #{projects[0]} was found"
+        @name = projects[0].split('.xcodeproj')[0]
+        @target = projects[0].split('.xcodeproj')[0]
+        puts "target: #{@target}  name: #{@name}"
       end
 
       FileUtils.cd current_dir
@@ -67,6 +70,23 @@ module Three20
       puts "expanded: #{File.expand_path(relative_path)}"
     end
 
+    # TODO - Fix this. giant ruby violation
+    def __str__
+      #str(self.name)+" target:"+str(self.target)+" guid:"+str(self._guid)+" prodguid: "+self._product_guid+" prodname: "+self._product_name
+    end
+
+
+    def uniqueid
+      @name << ':' << @target
+    end
+
+    def guid
+      if @guid.nil?
+        self.dependencies
+      end
+      @guid
+    end
+
     # Load the project data from disk.
     def get_project_data(path, project_name)
     #if self._project_data is None:
@@ -74,22 +94,28 @@ module Three20
     #logging.info("Couldn't find the project at this path:")
     #logging.info(self.path())
     #return None
+
+      puts "get_project_data"
+
       @project_data = nil
-      
+
       xcode_project = path << "/" << project_name << "/project.pbxproj"
       puts "xcode_project = #{xcode_project}"
 
-      file = File.new(xcode_project, "r")
+      #file = File.open(xcode_project, "rb")
+      @project_data = File.open(xcode_project, 'rb') { |f| f.read }
+      #@project_data = file.sysread
+      #counter = 1
+      #while (line = file.gets)
+      #  puts "#{counter}: #{line}"
+      #  counter = counter + 1
+      #end
+      #file.close
+      #s = IO.read(xcode_project)
+      #puts "s = #{s}"
+      #puts "project data #{@project_data}"
 
-      counter = 1
-      while (line = file.gets)
-        puts "#{counter}: #{line}"
-        #@project_data << line
-        counter = counter + 1
-      end
-      file.close
-
-      
+      self.dependencies
 
     #project_file = open(self.path(), 'r')
     #self._project_data = project_file.read()
@@ -109,8 +135,87 @@ module Three20
 
     # Get and cache the dependencies for this project.
     def dependencies
+      if @deps.nil?
 
+        # Get build phases
+        build_phases = @project_data.match('([A-Z0-9]+) \/\* ' << @target.to_s << ' \*\/ = {\n[ \t]+isa = PBXNativeTarget;(?:.|\n)+?buildPhases = \(\n((?:.|\n)+?)\);')
 
+        puts "build_phases = #{build_phases}"
+
+        if build_phases.nil?
+          puts "This is fatal: Unable to find the build phases from your target at: "  # TODO - add path as a ivar
+          #raise error?
+          return nil
+        end
+
+        #(self._guid, buildPhases, ) = result.groups()
+        # not sure what this line is supposed to do
+        puts build_phases.to_s.index(' /*')
+        idx = build_phases.to_s.index(' /*').to_i
+        @guid = build_phases.to_s[0..idx-1]
+        puts "guid: #{@guid}"
+        
+        # Get the build phases we care about.
+        match = nil
+        puts build_phases.to_s.index(' /* Resources */,')
+        #match = re.search('([A-Z0-9]+) \/\* Resources \*\/', buildPhases)
+        unless match.nil?
+          #(self._resources_guid, ) = match.groups()
+        else
+          @resources_guid = nil
+        end
+        
+        #match = re.search('([A-Z0-9]+) \/\* Frameworks \*\/', buildPhases)
+        if match.nil?
+          puts "Couldn't find the Frameworks phase from: " #+self.path()
+          puts "Please add a New Link Binary With Libraries Build Phase to your target"
+          puts "Right click your target in the project, Add, New Build Phase,"
+          puts " \"New Link Binary With Libraries Build Phase\""
+          return nil
+        end
+
+        @frameworks_guid = nil
+        #(self._frameworks_guid, ) = match.groups()
+
+        # Get the dependencies
+        #result = re.search(re.escape(self._guid)+' \/\* '+re.escape(self.target)+' \*\/ = {\n[ \t]+isa = PBXNativeTarget;(?:.|\n)+?dependencies = \(\n((?:[ \t]+[A-Z0-9]+ \/\* PBXTargetDependency \*\/,\n)*)[ \t]*\);\n',project_data)
+
+        if result.nil?
+          puts "Unable to get dependencies from: "#+self.path()
+          #return None
+          return nil
+        end
+        
+        #(dependency_set, ) = result.groups()
+        #dependency_guids = re.findall('[ \t]+([A-Z0-9]+) \/\* PBXTargetDependency \*\/,\n', dependency_set)
+
+        # Parse the dependencies
+        dependency_names = []
+
+        #for guid in dependency_guids:
+        #  result = re.search(guid+' \/\* PBXTargetDependency \*\/ = \{\n[ \t]+isa = PBXTargetDependency;\n[ \t]*name = (["a-zA-Z0-9\.\-]+);', project_data)
+
+        unless result.nil?
+          #(dependency_name, ) = result.groups()
+          #dependency_names.append(dependency_name)
+        end
+
+        @deps = dependency_names
+
+        # Get the product guid and name.
+        #result = re.search(re.escape(self._guid)+' \/\* '+re.escape(self.target)+' \*\/ = {\n[ \t]+isa = PBXNativeTarget;(?:.|\n)+?productReference = ([A-Z0-9]+) \/\* (.+?) \*\/;', project_data)
+
+        if result.nil?
+          #logging.error("Unable to get product guid from: "+self.path())
+          #return None
+        end
+
+        #(self._product_guid, self._product_name, ) = result.groups()
+
+      else
+        @deps
+      end
+      @deps
     end
 
 
@@ -120,7 +225,7 @@ module Three20
     #
     # Returns: <default_guid> if a line was added.
     # Otherwise, the existing guid is returned.
-    def add_build_file
+    def add_build_file(name, file_ref_hash, default_guid)
 
     end
 
@@ -130,32 +235,32 @@ module Three20
     #
     # Returns: <default_guid> if a line was added.
     # Otherwise, the existing guid is returned.
-    def add_file_reference
+    def add_file_reference(name, file_type, default_guid, rel_path, source_tree)
 
     end
 
     # Add a file to the given PBXGroup.
     #
     # <guid> /* <name> */,
-    def add_file_to_group
+    def add_file_to_group(name, guid, group)
       
     end
 
     # Add a file to the Frameworks PBXGroup.
     #
     # <guid> /* <name> */,
-    def add_file_to_frameworks
+    def add_file_to_frameworks(name, guid)
 
     end
 
     # Add a file to the Resources PBXGroup.
     #
     # <guid> /* <name> */,
-    def add_file_to_resources
+    def add_file_to_resources(name, guid)
       
     end
 
-    def add_file_to_phase
+    def add_file_to_phase(name, guid, phase_guid, phase)
       
     end
 
@@ -168,12 +273,38 @@ module Three20
       
     end
 
+    def get_hash_base(uniquename)
+      examplehash = '320FFFEEEDDDCCCBBBAAA000'
+      #uniquehash = hashlib.sha224(uniquename).hexdigest().upper()
+      #uniquehash = uniquehash[:len(examplehash) - 4]
+      #'320'+uniquehash
+    end
+
     def add_framework(framework)
+      #tthash_base = self.get_hash_base(framework)
+      #fileref_hash = self.add_filereference(framework, 'frameworks', tthash_base+'0', 'System/Library/Frameworks/'+framework, 'SDK_ROOT')
+      #libfile_hash = self.add_buildfile(framework, fileref_hash, tthash_base+'1')
+
+      #self.add_file_to_frameworks(framework, fileref_hash)
+
+      #self.add_file_to_frameworks_phase(framework, libfile_hash)
       
     end
 
     def add_bundle
-      
+      #tthash_base = self.get_hash_base('Three20.bundle')
+      #project_path = os.path.dirname(os.path.abspath(self.xcodeprojpath()))
+      #build_path = os.path.join(Paths.src_dir, 'Three20.bundle')
+      #rel_path = relpath(project_path, build_path)
+
+      #fileref_hash = self.add_file_reference('Three20.bundle', 'plug-in', tthash_base+'0', rel_path, 'SOURCE_ROOT')
+
+      #libfile_hash = self.add_build_file('Three20.bundle', fileref_hash, tthash_base+'1')
+
+      #self.add_file_to_resources('Three20.bundle', fileref_hash)
+
+      #self.add_file_to_resources_phase('Three20.bundle', libfile_hash)
+
     end
 
     def add_dependency(dep)
